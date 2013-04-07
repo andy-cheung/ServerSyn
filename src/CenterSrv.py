a325d345f82de93b65bc5d251345f15344d9df16
andy.cheung <andy@andycheungtekiMacBook-Pro.local>
2013-04-07 15:50:25 +0800

    基本完成功能，并简单测试了

--- src/CenterSrv.py
+++ src/CenterSrv.py
@@ -2,18 +2,77 @@
 import socket
 import sys
 import os
+import thread
+import copy
 
-if __name__ == '__main__':
+def sendcmd(ip, cmd):
 	port = 21000
-	if len(sys.argv) < 1:
-		print("arg less then 1")
-		os.exit(1)
-
-	s = socket.socket()
-	s.connect((sys.argv[1], port))
-	s.send("update")
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((ip, port))
+	s.send(cmd)
 	print(s.recv(1024))
 	#close socket finally
 	s.close()
 
+def waitforret(iplist):
+	allip = copy.deepcopy(iplist)
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a socket
+	port = 21001
+	s.bind(("0.0.0.0", port))
+	s.listen(10)
+	okcount, failcount = 0, 0
+	while True:
+		c, addr = s.accept()
+		ip = addr[0]
+		recvstr = c.recv(1024)
+		c.close()
+
+		strlist = recvstr.split(",")
+		ret = "ok"
+		cmd = ""
+		if len(strlist) < 2:
+			ret = "fail"
+			failcount = failcount + 1
+		else:
+			cmd = strlist[0]
+			ret = strlist[1]
+			okcount = okcount + 1
+		# recv a cmd, remove it from list 
+		allip.remove(ip)
+
+		# all return , this thread over
+		if len(allip) <= 0:
+			break
+
+	print "all recv result, succ:%d, fail:%d" % (okcount, failcount)	
+	s.close()
+
+def startthread(cmd, allip) :
+	thread.start_new_thread(waitforret, (allip, ))
+
+if __name__ == '__main__':
+	#arg1 is ip, arg2 is the cmd
+	# if len(sys.argv) < 2:
+	# 	print("arg less then 2")
+	# 	os.exit(1)
+	while True:
+		cmd = sys.stdin.readline()[:-1]
+		if cmd == "exit":
+			sys.exit(0)
+		elif cmd == "update":
+			# update file every server
+			# load ip list from file
+			allip = []
+			fp = open("iplist.txt", "r")		
+			for line in fp.readlines():  
+				line = line.strip('\n')
+				allip.append(line)
+
+			fp.close()
+
+			allipcpy = copy.deepcopy(allip)
+			startthread(cmd, allipcpy)
+			for ip in allip:
+				sendcmd(ip, cmd)	
 
--- src/NoteSrv.py
+++ src/NoteSrv.py
@@ -26,29 +26,41 @@ def startSrv():
 	sys.stdout.flush()
 	sys.stderr.flush()
 
-def process(cmd):
+def process(cmd, ip):
 	print "cmd:" + cmd
 	if cmd == "update":
+		# update file from file center
 		print("update")
+		os.system("./update.sh")
 	elif cmd == "start":
+		# start all game server
 		print("start")
+	# call back the center server 
+	ret = "ok"
+	sendresult(cmd, ip, ret)
 
+def sendresult(cmd, ip, ret):
+	port = 21001
+	s = socket.socket()
+	s.connect((ip, port))
+	s.send(cmd + "," + ret)
+	s.close()	
 
 if __name__ == '__main__':
 	startSrv()
 	print("startSrv success!")
 
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a socket
-	host = socket.gethostname()
 	port = 21000
 	s.bind(("0.0.0.0", port))
 	s.listen(10)
 	while True:
 		c, addr = s.accept()
+		ip = addr[0]
 		cmd = c.recv(1024)
 		c.send("recv ok")
 		c.close()
 
-		print("recv cmd:" + cmd)
+		print "recv cmd:%s, from:%s" % (cmd, ip)
 		#process the cmd
-		process(cmd)
+		process(cmd, ip)
